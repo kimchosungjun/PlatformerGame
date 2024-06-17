ba03422aabb7ada00f3edc24d4cbfe6f6b4bd105
sung jun <chosung0531@naver.com>
2024-06-17 14:36:38 +0900

    -0616

--- Assets/Scripts/MoveController.cs
+++ Assets/Scripts/MoveController.cs
@@ -9,8 +9,9 @@ public class MoveController : MonoBehaviour
     
     [Header("플레이어 이동 및 점프")]
     Rigidbody2D rb;
-    CapsuleCollider2D coll;
+    BoxCollider2D coll;
     Animator anim;
+
     Vector3 moveDir;
     float verticalVelocity = 0f; // 수직으로 떨어지는 힘
     [SerializeField] float jumpForce;
@@ -18,35 +19,27 @@ public class MoveController : MonoBehaviour
     [SerializeField] float groundCheckLength;
     [SerializeField] Color colorGroundCheck;
     [SerializeField] bool isGround;
+
+    bool isJump = false; // 점프 버그를 막기 위함
+
+    [SerializeField] bool isOnGround = false;
+    [SerializeField] float groundCheckDistance;
+    [SerializeField] LayerMask groundLayer;
+
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
-        coll = GetComponent<CapsuleCollider2D>();
+        coll = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         CheckGround();
-        Moving();
-    }
-
-    private bool CheckGround()
-    {
-        // tag는 string
-        // layer는 int 단, 일반적인 int는 아님
-        // layer의 번호는 2^n을 뜻함
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, groundCheckLength, LayerMask.GetMask("Ground"));
-        if (hit)
-        {
-            isGround = true;
-        }
-        else
-        {
-            isGround = false;
-        }
-        return isGround;
+        CheckGravity();
+        Move();
+        Jump();
     }
 
     private void OnDrawGizmos()
@@ -57,10 +50,69 @@ public class MoveController : MonoBehaviour
         // Handles는 using unityeditor를 해야함
     }
 
-    private void Moving()
+    private void CheckGround()
+    {
+        isOnGround = false;
+        if (verticalVelocity > 0f)
+            return;
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, groundCheckLength, LayerMask.GetMask("Ground"));
+        if (hit)
+            isOnGround = true;
+        //isJump = !isOnGround;
+    }
+
+    private void Move()
     {
         moveDir.x = Input.GetAxisRaw("Horizontal") * moveSpeed; // -1 or 1
         moveDir.y = rb.velocity.y;
         rb.velocity = moveDir;
+    }
+    private void Jump()
+    {
+        if (!isOnGround)
+        {
+            return;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            isJump = true;
+
+        }
+
+        //if (isOnGround && isJump && Input.GetKeyDown(KeyCode.Space))
+        //{
+        //    rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse); // 지긋이 미는 힘, 벡터만 넣었을 때
+        //    isJump = true;                                // : 이단 점프는 addforce로 구현 불가
+        //}
+    }
+
+    private void CheckGravity()
+    {
+        if (!isOnGround)
+        {
+            verticalVelocity += Physics2D.gravity.y * Time.deltaTime;
+            // 물리와 관련된(Rigidbody) 것에는 Time.deltaTime을 사용하지 않아도 된다.
+            if (verticalVelocity < -10f)
+            {
+                verticalVelocity = -10f;
+            }
+        }
+        else if (isJump)
+        {
+            isJump = false;
+            verticalVelocity = jumpForce;
+        }
+        else if (isOnGround)
+        {
+            verticalVelocity = 0f;
+        }
+        rb.velocity = new Vector2(rb.velocity.x, verticalVelocity);
+    }
+
+    private void DoAnimation()
+    {
+        //anim.SetInteger("Horizontal",(int)moveDir.x);
+        //anim.SetBool("isOnGround",isOnGround);
     }
 }
